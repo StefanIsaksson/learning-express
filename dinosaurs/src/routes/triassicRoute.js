f214b9d3faf6f720aaac97b3a0705ad2c230f633
Stefan Isaksson <stefan.foo.isaksson@gmail.com>
2019-01-29 00:38:33 +0100

    Added dino page

---
+++ dinosaurs/src/routes/triassicRoute.js
@@ -0,0 +1,81 @@
+const express = require('express');
+
+const triassicRouter = express.Router();
+
+var triassicDinosaurs = [
+  {
+      "id": 1,
+      "name": "Saturnalia",
+      "type": "Sauropodomorph",
+      "length": "1.5m",
+      "mya": "233",
+      "description": "Herbivorous and of the earliest of all dinosaurs.",
+      "picture" : "https://upload.wikimedia.org/wikipedia/commons/0/01/Saturnalia_NT_small.jpg"
+  },
+  {
+      "id": 2,
+      "name": "Staurikosaurus",
+      "type": "Saurischian",
+      "length": "2.25m",
+      "mya": "233",
+      "description": "Small meat eater. One of the earliest dinosaurs.",
+      "picture" : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Human-staurikosaurus_size_comparison.svg/320px-Human-staurikosaurus_size_comparison.svg.png"
+  },
+  {
+      "id": 3,
+      "name": "Thecodontosaurus",
+      "type": "Sauropodomorph (an early sauropod)",
+      "length": "2m",
+      "mya": "203-201",
+      "description": "Relatively small Triassic dinosaur.",
+      "picture" : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Thecodontosaurus_Scale.svg/320px-Thecodontosaurus_Scale.svg.png"
+  },
+  {
+      "id": 4,
+      "name": "Riojasaurus",
+      "type": "Sauropodomorph (an early sauropod)",
+      "length": "10m",
+      "mya": "221-210",
+      "description": "Large sauropodomorph.",
+      "picture" : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Riojasaurus_Scale.svg/800px-Riojasaurus_Scale.svg.png"
+  }
+];
+
+triassicRouter.route('/')
+    .get((req, res) => {
+        res.render(
+            'triassic',
+            {
+                nav: [
+                    { link: "/", title: 'Home' },
+                    { link: "/triassic", title: 'Triassic' },
+                    { link: "/jurassic", title: 'Jurassic' },
+                    { link: "/cretaceous", title: 'Cretaceous' }],
+                title: "Triassic",
+                triassicDinosaurs
+            }
+        );
+    });
+
+triassicRouter.route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        console.log(id);
+        console.log(triassicDinosaurs);
+        const dino = triassicDinosaurs.find(x => parseInt(x.id) === parseInt(id));
+        console.log(dino);
+        res.render(
+          'dinosaur',
+          {
+              nav: [
+                  { link: "/", title: 'Home' },
+                  { link: "/triassic", title: 'Triassic' },
+                  { link: "/jurassic", title: 'Jurassic' },
+                  { link: "/cretaceous", title: 'Cretaceous' }],
+              title: "Triassic",
+              dino : dino
+          }
+        );
+    });
+
+  module.exports = triassicRouter;
